9a462b8cb79a82fe58a49db37377097114cda97a
bsfelizardo <bsfelizardo@up.edu.ph>
2022-06-13 23:39:20 +0800

    Add connection to auth service

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/user', proxy('http://localhost:3001'))
+app.use('/user', proxy('http://localhost:3002'))
+app.use('/auth', proxy('http://localhost:3003'))
 
 app.use('/', (req, res) => {
     res.json({
@@ -15,7 +16,7 @@ app.use('/', (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`API Gateway started on Port ${PORT}`)
