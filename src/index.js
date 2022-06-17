458466b2243fd6be9b2de88223ba759fdb6d7790
bsfelizardo <bsfelizardo@up.edu.ph>
2022-06-17 23:07:03 +0800

    Move entry file inside src/

---
+++ src/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', proxy('http://localhost:3002'))
+app.use('/auth', proxy('http://localhost:3003'))
+
+app.use('/', async (req, res) => {
+    console.log('hereeeeeeeeeeeeeeeee');
+    console.log(req.user);
+    res.json({
+                message: 'Inside API Gateway'
+            })
+})
+
+// app.use('/', async (req, res) => {
+//     // console.log(req.headers);
+//     // cookies = req.headers.cookie
+//     // console.log(cookies);
+//     const { data } = await axios({
+//         method: 'get',
+//         url: 'http://localhost:3003/getUser',
+//         headers: {
+//             cookie: req.headers.cookie
+//         }
+//     })
+//     console.log(data.User);
+//     res.json({
+//         message: 'Inside API Gateway'
+//     })
+// })
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`API Gateway started on Port ${PORT}`)
+})
